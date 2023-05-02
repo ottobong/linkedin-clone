24deec8d6f566bf656cc22421eca143f37cf6d16
Ottobong <clemency50@gmail.com>
2023-05-02 07:02:12 +0100

    completed header component

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import './Header.css';
-import { Search } from '@mui/icons-material';
+import { BusinessCenterRounded, HomeRounded, NotificationsRounded, Search, SupervisorAccountRounded, TextsmsRounded } from '@mui/icons-material';
+import HeaderOption from './HeaderOption';
 
 function Header() {
   return (
     <div className='header'>
-      <h1>This is a homepage</h1>
 
       {/* Left part of the header */}
       <div className="header__left">
-        <img src="" alt="" />
+        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" 
+        alt="" 
+        />
 
         <div className="header__search">
           {/* Search icon */}
-          <input type="text" />
           <Search />
+          <input type="text" />
+          
         </div>
       </div>
 
       {/* Right part of the header */}
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeRounded} title='Home'/>
+        <HeaderOption Icon={SupervisorAccountRounded} title='My Network'/>
+        <HeaderOption Icon={BusinessCenterRounded} title='Jobs'/>
+        <HeaderOption Icon={TextsmsRounded} title='Messaging'/>
+        <HeaderOption Icon={NotificationsRounded} title='Notifications'/>
+        <HeaderOption avatar='https://media.licdn.com/dms/image/D4D35AQEZGau_CXBAbA/profile-framedphoto-shrink_100_100/0/1650353307849?e=1683612000&v=beta&t=rbeRcRQHQWZzZKIu_8trtZ93ycsklezjLIVxJe5c1vM' 
+        title='Me'/>
+      </div>
     </div>
   )
 }
